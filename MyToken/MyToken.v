45ae5dc5ef460cbf706e0d7cf2f07deb5a474489
R1kM <aymeric.fr@hotmail.fr>
2016-03-16 22:16:58 +0100

    Redefining MyToken using Maps

--- MyToken/MyToken.v
+++ MyToken/MyToken.v
@@ -1,13 +1,19 @@
+Require Import Coq.FSets.FMapWeakList.
 Require Import String.
+Require Import OrderedTypeEx.
 
-Variable address : Type.
+Module AddrMap := FMapWeakList.Make(Nat_as_OT).
+Definition MapAddrIntType := AddrMap.t nat.
 
 Record MyToken := mytoken {
   name: string;
   symbol: string;
   decimals: nat;
 
-  balanceOf: address -> nat;
-  allowance : address -> address -> nat;
-  spentAllowance : address -> address -> nat;
+  balanceOf: AddrMap.t nat;
+  allowance: AddrMap.t MapAddrIntType;
+  spentAllowance: AddrMap.t MapAddrIntType;
 }.
+
+
+
