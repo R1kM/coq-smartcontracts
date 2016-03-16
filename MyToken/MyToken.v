0f84ec2b4cff110fab7b1c8323f22e49c9f9e0c6
R1kM <aymeric.fr@hotmail.fr>
2016-03-16 21:04:44 +0100

    Adding complete definition of fields of the class

--- MyToken/MyToken.v
+++ MyToken/MyToken.v
@@ -1,7 +1,13 @@
 Require Import String.
 
+Variable address : Type.
+
 Record MyToken := mytoken {
   name: string;
   symbol: string;
   decimals: nat;
+
+  balanceOf: address -> nat;
+  allowance : address -> address -> nat;
+  spentAllowance : address -> address -> nat;
 }.
