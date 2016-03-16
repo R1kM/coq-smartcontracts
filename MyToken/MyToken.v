533d8a01637c24c7e398e024988ae40e2e2f1da2
R1kM <aymeric.fr@hotmail.fr>
2016-03-16 23:05:02 +0100

    Adding constructor for MyToken

--- MyToken/MyToken.v
+++ MyToken/MyToken.v
@@ -5,6 +5,8 @@ Require Import OrderedTypeEx.
 Module AddrMap := FMapWeakList.Make(Nat_as_OT).
 Definition MapAddrIntType := AddrMap.t nat.
 
+Definition sender := 0.
+
 Record MyToken := mytoken {
   name: string;
   symbol: string;
@@ -15,5 +17,7 @@ Record MyToken := mytoken {
   spentAllowance: AddrMap.t MapAddrIntType;
 }.
 
-
-
+Definition create_token (initialSupply:nat) (tokenName:string) (decimalUnits:nat) (tokenSymbol:string) :=
+  let startBalance := AddrMap.empty nat in
+  let actualBalance := AddrMap.add sender initialSupply startBalance in
+  mytoken tokenName tokenSymbol decimalUnits startBalance (AddrMap.empty MapAddrIntType) (AddrMap.empty MapAddrIntType).
